da06723f9d541ecf4794c2ceeb0574ac7da72600
Will Sheehan <wsheehan@bates.edu>
2016-11-06 21:59:32 -0500

    Read CSV data into list

--- lib/mix/tasks/open_stock.import_companies.ex
+++ lib/mix/tasks/open_stock.import_companies.ex
@@ -4,6 +4,11 @@ defmodule Mix.Tasks.OpenStock.ImportCompanies do
   @shortdoc "Import Stocks from S&P 500 from CSV file"
 
   def run(_args) do
-    Mix.shell.info ""
+    File.stream!("/Users/willsheehan/Desktop/sp_companies.csv") |>
+    CSV.decode(separator: ?\t) |>
+    Enum.map(fn row ->
+      stock_row = Enum.at(row, 0) |> String.split(",")
+      IO.puts Enum.at(stock_row, 0)
+    end)
   end
 end
