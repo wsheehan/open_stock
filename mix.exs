f04bc03725281db23be1ca216637a6ead00e8efb
Will Sheehan <wsheehan@bates.edu>
2016-11-07 14:42:31 -0500

    Add HTTPoison dependency

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule OpenStock.Mixfile do
   def application do
     [mod: {OpenStock, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :httpoison]]
   end
 
   # Specifies which paths to compile per environment.
@@ -38,7 +38,8 @@ defmodule OpenStock.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:csv, "~> 1.4.2"}]
+     {:csv, "~> 1.4.2"},
+     {:httpoison, "~> 0.10.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
