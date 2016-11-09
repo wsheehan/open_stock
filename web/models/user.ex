188369bb7cd434d39a8315525d03ea4f604fdc76
Will Sheehan <wsheehan@bates.edu>
2016-11-08 19:46:42 -0500

    User model with email and hashed password

---
+++ web/models/user.ex
@@ -0,0 +1,20 @@
+defmodule OpenStock.User do
+  use OpenStock.Web, :model
+
+  schema "users" do
+    field :email, :string
+    field :encrypted_password, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:email, :encrypted_password])
+    |> validate_required([:email, :encrypted_password])
+    |> unique_constraint(:email)
+  end
+end
