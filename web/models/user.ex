78164da6e727e4f19f894d86905039a027ace4cc
Will Sheehan <wsheehan@bates.edu>
2016-11-11 16:42:20 -0500

    Hash password in register changeset

--- web/models/user.ex
+++ web/models/user.ex
@@ -21,4 +21,24 @@ defmodule OpenStock.User do
     |> validate_required(@required_fields)
     |> unique_constraint(:email)
   end
+
+  def register_changeset(struct, params) do
+    struct
+    |> changeset(params)
+    |> cast(params, ~w(password)a, [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> hash_password
+  end
+
+  defp hash_password(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true,
+                      changes: %{password: password}} ->
+        put_change(changeset,
+                   :password_hash,
+                   Comeonin.Bcrypt.hashpwsalt(password))
+      _ ->
+        changeset
+    end
+  end
 end
