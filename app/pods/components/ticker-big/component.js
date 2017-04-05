fb2ac8a18d0a01ad78020081e590fafa03a91cb4
Will Sheehan <wsheehan@bates.edu>
2017-04-05 17:03:33 -0400

    Ticker big first pass

--- app/pods/components/ticker-big/component.js
+++ app/pods/components/ticker-big/component.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['ticker-big', 'col-sm-2'],
+
   click() {
     this.sendAction('listenToCompany', this.get('ticker'));
   }
