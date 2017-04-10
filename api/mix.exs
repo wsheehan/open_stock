3f571843c67a50671b6b3d4f2d256f5f5d04a735
Will Sheehan <wsheehan@bates.edu>
2017-04-10 16:27:39 -0400

    Update ecto and postgrex

--- api/mix.exs
+++ api/mix.exs
@@ -32,8 +32,8 @@ defmodule OpenStock.Mixfile do
   defp deps do
     [{:phoenix, "~> 1.2.1"},
      {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
+     {:phoenix_ecto, "~> 3.2"},
+     {:postgrex, "~> 0.13.2"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
